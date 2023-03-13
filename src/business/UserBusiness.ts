3e98357068d454c469c126010b2745273bcc23c8
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2023-03-13 20:10:16 -0300

    FEAT: Instalação do uuid e criação do gerador de id

--- src/business/UserBusiness.ts
+++ src/business/UserBusiness.ts
@@ -1,14 +1,13 @@
 import { UserInputDTO } from "../model/user/userInputDTO";
 
 export class UserBusiness{
-    createUser =async (user:UserInputDTO) => {
+    createUser =async (input:UserInputDTO) => {
         
-        const {name, email, password, role} = user;
+        const {name, email, password, role} = input;
 
         if(!name && !email && !password && !role){
             throw new Error("Insira o nome, email, password e role!")
         }
 
-        
     }
 }
