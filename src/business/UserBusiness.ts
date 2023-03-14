854902e33d9fdb94012c8c9c12f95ec5f0e6fb2d
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2023-03-13 21:10:06 -0300

    FEAT: Teste dos erros

--- src/business/UserBusiness.ts
+++ src/business/UserBusiness.ts
@@ -8,6 +8,7 @@ import { UserInputDTO } from "../model/user/userInputDTO";
 import { InvalidEmail, InvalidPassword, InvalidRole,
         NotNullEmail, NotNullName, NotNullPassword, NotNullRole } 
 from "../error/UserError";
+import { CustomError } from "../error/CustomError";
 
 const authenticator = new Authenticator()
 const userDatabase = new UserDatabase()
@@ -16,42 +17,48 @@ const hashManager = new HashManager();
 
 export class UserBusiness{
     createUser =async (input:UserInputDTO) => {
-        
-        const {name, email, password, role} = input;
 
-        if(!name){
-            throw new NotNullName()
-        }else if(!email){
-            throw new NotNullEmail()
-        }else if(!password){
-            throw new NotNullPassword()
-        }else if(!role){
-            throw new NotNullRole()
-        }else if(!email.includes("@")){
-            throw new InvalidEmail();
-        }else if(password.length <=6){
-            throw new InvalidPassword()
+        try{
+            const {name, email, password, role} = input;
+    
+            if(!name){
+                throw new NotNullName()
+            }else if(!email){
+                throw new NotNullEmail()
+            }else if(!password){
+                throw new NotNullPassword()
+            }else if(!role){
+                throw new NotNullRole()
+            }else if(!email.includes("@")){
+                throw new InvalidEmail();
+            }else if(password.length <=6){
+                throw new InvalidPassword()
+            }
+    
+            if(role.toUpperCase() != Role.ADMIN && role.toUpperCase() != Role.NORMAL){
+                throw new InvalidRole()
+            }
+    
+            const id: string = generateId()
+    
+            const hashPassword: string = await hashManager.hash(password)
+    
+            const user: User={
+                id,
+                name,
+                email,
+                password: hashPassword,
+                role
+            }
+            const token = authenticator.generateToken({id, role})
+            
+            await userDatabase.createUser(user)
+            
+            return token
+        }catch(error:any){
+            throw new CustomError(error.message, 400)
         }
 
-        if(role.toUpperCase() != Role.ADMIN && role.toUpperCase() != Role.NORMAL){
-            throw new InvalidRole()
-        }
-
-        const id: string = generateId()
-
-        const hashPassword: string = await hashManager.hash(password)
-
-        const user: User={
-            id,
-            name,
-            email,
-            password: hashPassword,
-            role
-        }
-
-        await userDatabase.createUser(user)
-        
-        const token = authenticator.generateToken({id, role})
-        return token
     }
+        
 }
