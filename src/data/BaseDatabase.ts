2473d677e8ccf83ffd6d2bda108017493695ee45
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2023-03-13 21:31:15 -0300

    FIX: Alteração do arquivo app para index

--- src/data/BaseDatabase.ts
+++ src/data/BaseDatabase.ts
@@ -6,13 +6,14 @@ dotenv.config();
 
 export abstract class BaseDatabase {
     protected static connection = knex({
-        client: 'mysql',
-        connection: {
-           host: process.env.DB_HOST,
-           user: process.env.DB_USER,
-           password: process.env.DB_PASSWORD,
-           database: process.env.DB_SCHEMA,
-           port: 3306,
-        }
+      client: "mysql",
+      connection: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_SCHEMA,
+        port: 3306,
+        multipleStatements: true,
+      },
      })
 }
