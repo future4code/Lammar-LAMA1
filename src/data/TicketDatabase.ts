a63f1c6f985e65c9c271af329f0873d705aea1e1
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2023-03-17 12:35:32 -0300

    FIX: Alteração na tabela, inserindo a coluna de id do show

--- src/data/TicketDatabase.ts
+++ src/data/TicketDatabase.ts
@@ -1,11 +1,21 @@
+import { Ticket } from "../model/ticket/Ticket";
 import { BaseDatabase } from "./BaseDatabase";
 
 export class TicketDatabase extends BaseDatabase{
-    createTicket = async(ticket) =>{
+    createTicket = async(ticket: Ticket) =>{
         try{
 
-        }catch{
+            await TicketDatabase.connection
+            .insert({
+                id: ticket.id,
+                name: ticket.name,
+                price: ticket.price,
+                qty_stock: ticket.qty_stock,
+                qty_sold: ticket.qty_sold
+            }).into("TICKET_LAMA")
 
+        }catch(error:any){
+            throw new Error(error.message)
         }
     }
 }
